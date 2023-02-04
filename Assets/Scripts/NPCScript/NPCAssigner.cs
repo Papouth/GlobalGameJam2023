d655473f5c258da26f9682bd4d9b06b7a7d0536d
Amenixx <serveurminequestfr@gmail.com>
2023-02-04 15:19:57 +0100

    Update NPCAssigner.cs

--- Assets/Scripts/NPCScript/NPCAssigner.cs
+++ Assets/Scripts/NPCScript/NPCAssigner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//à mettre dans un empty random dans la scène
 public class NPCAssigner : MonoBehaviour
 {
     public NPC grabbedNPC;

3d9068c78f1c353eb04e8f1f2c17140d76c30676
Papouth <tawairpofficiel@gmail.com>
2023-02-05 16:45:48 +0100

    eazgrh

--- Assets/Scripts/TheEnd.cs
+++ Assets/Scripts/TheEnd.cs
@@ -6,15 +6,24 @@ using UnityEngine.SceneManagement;
 public class TheEnd : MonoBehaviour
 {
     public Arbre arbre;
+    private float timeFin;
+    private float timerFin;
 
     private void Start()
     {
-        arbre = FindObjectOfType<Arbre>();  
+        arbre = FindObjectOfType<Arbre>();
+        timerFin = 300f;
     }
 
     private void Update()
     {
-       if (arbre.arbreLife <= 0)
+        if (arbre.arbreLife <= 0)
+        {
+            SceneManager.LoadScene("Xblaze");
+        }
+
+        timeFin += Time.deltaTime;
+        if (timeFin > timerFin)
         {
             SceneManager.LoadScene("Xblaze");
         }
